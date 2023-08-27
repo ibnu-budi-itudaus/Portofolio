3b7b721020b8d4356cde65e406e7f4bda9173884
ibnu-budi-itudaus <firdaus041195@gmail.com>
2023-08-27 11:52:13 +0700

    add contact

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ function toggleDetail(e) {
    
 
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#in_email")
+    const subject = $("#in_subject")
+    const message = $("#in_message")
+
+
+    if(!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Message is required")
+
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
